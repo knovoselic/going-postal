c104b67787e6957ad6f5dd7782affd30592b4460
Kristijan Novoselic <kristijan.novoselic@gmail.com>
2015-05-07 20:25:34 +0200

    Installed devise

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'puma'
 gem 'mongoid'
 gem 'rubocop', require: false
 gem 'rubocop-rspec', require: false
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module GoingPostalWeb
     # config.i18n.default_locale = :de
 
     config.logger = Logger.new(STDOUT)
+
+    config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
   end
 end
