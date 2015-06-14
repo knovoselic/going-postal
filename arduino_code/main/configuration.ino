5307727cb677572ce3a417998576ef2c243f420e
Kristijan Novoselic <kristijan.novoselic@gmail.com>
2015-06-14 20:33:27 +0200

    Implemented web server that sends configuration page to the client

--- arduino_code/main/configuration.ino
+++ arduino_code/main/configuration.ino
@@ -1,5 +1,9 @@
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include "FlashStream.h"
+
 extern "C" {
 #include <user_interface.h>
 }
@@ -7,9 +11,24 @@ extern "C" {
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 144, 1);
 DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+String domainName = "going-postal.config";
+
+void handleNotFound()
+{
+  Serial.println(webServer.uri());
+  if (webServer.uri() == "/favicon.ico")
+  {
+    webServer.send(404, "text/plain", "<h1>Not found.</h1>");
+  } else {
+    webServer.sendHeader("Location", "http://" + domainName, true);
+    webServer.send(301, "", "");
+  }
+}
 
 void configurationSetup() {
   Serial.begin(115200);
+  Serial.println();
   WiFi.mode(WIFI_AP_STA);
   WiFi.disconnect();
 
@@ -17,9 +36,35 @@ void configurationSetup() {
   WiFi.softAP("GoingPostal configuration");
 
   dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-  dnsServer.start(DNS_PORT, "going-postal.config", apIP);
-}
+  dnsServer.start(DNS_PORT, domainName, apIP);
 
+  webServer.onNotFound(handleNotFound);
+  webServer.on ( "/", []() {
+    FlashStream htmlStream((PGM_P)html);
+    webServer.streamFile(htmlStream, "text/html");
+  } );
+
+  webServer.begin();
+  Serial.println("Done.");
+}
+int start, duration;
 void configurationLoop() {
+  start = millis();
   dnsServer.processNextRequest();
+  duration = millis() - start;
+  if (duration > 2)
+  {
+    Serial.print("DNS took: ");
+    Serial.print(duration);
+    Serial.println("ms.");
+  }
+  start = millis();
+  webServer.handleClient();
+  duration = millis() - start;
+  if (duration > 2)
+  {
+    Serial.print("Web took: ");
+    Serial.print(duration);
+    Serial.println("ms.");
+  }
 }
--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -1,3 +1,4 @@
+#include <pgmspace.h>
 void (*loopFunction)();
 
 void setup() {
