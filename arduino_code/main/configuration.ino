303c17634a1aad3768279d6dd696558edb7969d4
Kristijan Novoselic <kristijan.novoselic@gmail.com>
2015-06-22 19:46:36 +0200

    /wifi/status now can be called multiple times, added /restart call

--- arduino_code/main/configuration.ino
+++ arduino_code/main/configuration.ino
@@ -15,6 +15,7 @@ DNSServer dnsServer;
 ESP8266WebServer webServer(80);
 String domainName = "going-postal.config";
 long shouldRestart = 0;
+short wifiStatus;
 
 void handleNotFound()
 {
@@ -29,8 +30,10 @@ void handleNotFound()
 
 void handleIndex()
 {
+  ETS_UART_INTR_DISABLE();
   FlashStream htmlStream((PGM_P)html);
   webServer.streamFile(htmlStream, "text/html");
+  ETS_UART_INTR_ENABLE();
 }
 
 void validateWiFiConfig()
@@ -43,42 +46,33 @@ void validateWiFiConfig()
   }
   String password = webServer.arg("password");
 
+  wifiStatus = -1;
+  webServer.send(200, "text/plain", "");
+  delay(1000);
   if (password == "")
   {
     WiFi.begin(ssid.c_str());
   } else {
     WiFi.begin(ssid.c_str(), password.c_str());
   }
-  webServer.send(200, "text/plain", "");
-  Serial.println("config done");
 }
 
 void getWiFiStatus()
 {
-  int status = WiFi.status();
-  Serial.println("got status");
-  Serial.println(status);
-  switch(status)
+  if (wifiStatus == -1)
   {
-    case WL_DISCONNECTED:
-      webServer.send(202, "text/plain", "");
-      return;
+    wifiStatus = WiFi.waitForConnectResult();
+  }
+  switch(wifiStatus)
+  {
     case WL_CONNECTED:
-    {
-      saveConfigAndRestart();
-      shouldRestart = millis();
-      Serial.println("Count start");
-      webServer.send(200, "text/plain", "Success.");
+      webServer.send(200, "text/plain", "");
+      saveConfig();
       return;
-    }
     case WL_NO_SSID_AVAIL:
-    {
-      String ssid = webServer.arg("ssid");
-      ssid.trim();
-      webServer.send(503, "text/plain", "Could not find a WiFi network named \""
-      + ssid + "\". Please check your input and try agian.");
+      webServer.send(503, "text/plain", "Could not find specified WiFi network."
+      " Please check your input and try again.");
       break;
-    }
     case WL_CONNECT_FAILED:
     case WL_CONNECTION_LOST:
       webServer.send(503, "text/plain", "Unable to connect to WiFi. Please make"
@@ -93,7 +87,7 @@ void getWiFiStatus()
   WiFi.disconnect();
 }
 
-void saveConfigAndRestart()
+void saveConfig()
 {
   EEPROM.begin(1);
   EEPROM.write(0, (int)BootMode::Sensor);
@@ -102,6 +96,7 @@ void saveConfigAndRestart()
 
 void configurationSetup() {
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
   Serial.println();
 
   WiFi.mode(WIFI_AP_STA);
@@ -110,6 +105,7 @@ void configurationSetup() {
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("GoingPostal configuration");
 
+  dnsServer.setTTL(60 * 60);
   dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
   dnsServer.start(DNS_PORT, domainName, apIP);
 
@@ -117,18 +113,17 @@ void configurationSetup() {
   webServer.on("/", HTTP_GET, handleIndex);
   webServer.on("/wifi/config", HTTP_POST, validateWiFiConfig);
   webServer.on("/wifi/status", HTTP_POST, getWiFiStatus);
+  webServer.on("/restart", HTTP_POST, [](){
+    webServer.send(200, "text/plain", "OK.");
+    delay(1000);
+    ESP.restart();
+  });
   webServer.begin();
 
   Serial.println("Configuration setup done.");
 }
 
 void configurationLoop() {
-  if (shouldRestart && (millis() - shouldRestart) > 5000)
-  {
-    Serial.println("Restarting");
-    shouldRestart = 0;
-    ESP.reset();
-  }
   measure("DNS", []() { dnsServer.processNextRequest(); });
   measure("Web", []() { webServer.handleClient(); });
 }
