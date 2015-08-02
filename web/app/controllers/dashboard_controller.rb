972d22c1c38bd0cce918d8ac73134065ae82bdc8
Kristijan Novoselic <kristijan.novoselic@gmail.com>
2015-08-02 12:03:04 +0200

    Move authentication to base controller class

--- web/app/controllers/dashboard_controller.rb
+++ web/app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @events = Event.in(device_id: current_user.device_ids).limit(10).entries
   end
--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     redirect_to :dashboard if user_signed_in?
   end
